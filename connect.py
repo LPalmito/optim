22508a4b6b25b03fa490ba5da83e123fab413152
L Palmito <yannickmorel1@gmail.com>
2017-01-12 15:02:07 +0100

    Added images

--- connect.py
+++ connect.py
@@ -11,6 +11,7 @@ from CentraleSupelec import CSP
 import time
 import pprint
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 
 
 def generate(n, with_print=True):
@@ -82,7 +83,7 @@ def prettyprint(grid):
         print()
 
 
-def solve(M, with_print=True):
+def solve(M, with_print=True, maintain_AC=False):
     """
     :param M: the grid to be solved
     :return: solution
@@ -108,6 +109,8 @@ def solve(M, with_print=True):
 
     # Initialize solver
     P = CSP(domains)
+    if maintain_AC:
+        P.maintain_arc_consistency()
 
     # Top-bottom constraints
     for k in range(n**2-n):
@@ -143,27 +146,71 @@ def solve(M, with_print=True):
 
 def measure_perf(n_max, precision):
     """Measure the performances of the solve method and print a graph from it"""
-    durations, averages, minimums, maximums = {}, {}, {}, {}
-    for k in range(1, n_max):
-        durations[k] = []
-        count = 0
-        while count < precision:
-            grid = generate(k, with_print=False)
-            start = time.time()
-            hasResult = solve(grid, with_print=False)
-            end = time.time()
-            if hasResult:
-                duration = (end - start) * 1000
-                durations[k].append(duration)
-                count += 1
-        averages[k] = sum(durations[k]) / len(durations[k])
-        minimums[k] = min(durations[k])
-        maximums[k] = max(durations[k])
+    dur_no_AC, min_no_AC, avg_no_AC, max_no_AC = {}, {}, {}, {}
+    dur_with_AC, min_with_AC, avg_with_AC, max_with_AC = {}, {}, {}, {}
+    perfs = {}
+    for maintain_AC in {False, True}:
+        for k in range(1, n_max):
+            if not maintain_AC:
+                dur_no_AC[k] = []
+            else:
+                dur_with_AC[k] = []
+            count = 0
+            while count < precision:
+                grid = generate(k, with_print=False)
+                start = time.time()
+                hasResult = solve(grid, with_print=False, maintain_AC=maintain_AC)
+                end = time.time()
+                if hasResult:
+                    duration = (end - start) * 1000
+                    if not maintain_AC:
+                        dur_no_AC[k].append(duration)
+                    else:
+                        dur_with_AC[k].append(duration)
+                    count += 1
+            if not maintain_AC:
+                min_no_AC[k] = min(dur_no_AC[k])
+                avg_no_AC[k] = sum(dur_no_AC[k]) / len(dur_no_AC[k])
+                max_no_AC[k] = max(dur_no_AC[k])
+            else:
+                min_with_AC[k] = min(dur_with_AC[k])
+                avg_with_AC[k] = sum(dur_with_AC[k]) / len(dur_with_AC[k])
+                max_with_AC[k] = max(dur_with_AC[k])
+    perfs['min_no_AC'] = list(min_no_AC.values())
+    perfs['avg_no_AC'] = list(avg_no_AC.values())
+    perfs['max_no_AC'] = list(max_no_AC.values())
+    perfs['min_with_AC'] = list(min_with_AC.values())
+    perfs['avg_with_AC'] = list(avg_with_AC.values())
+    perfs['max_with_AC'] = list(max_with_AC.values())
+    print_perfs(n_max, perfs)
+
+
+def print_perfs(n_max, perfs):
+    # Create patches for the legend
+    min = mpatches.Patch(color='blue', label='Minimum')
+    avg = mpatches.Patch(color='green', label='Moyenne')
+    max = mpatches.Patch(color='red', label='Maximum')
+    no_AC = mpatches.Patch(color='blue', label='Sans arc consistance')
+    with_AC = mpatches.Patch(color='green', label='Avec arc consistance')
+    # Plot graphs
     plt.figure(1)
-    plt.title('Temps de résolution (en ms) en fonction de n')
-    plt.plot(list(range(1, n_max)), list(minimums.values()))
-    plt.plot(list(range(1, n_max)), list(averages.values()))
-    plt.plot(list(range(1, n_max)), list(maximums.values()))
+    plt.subplot(211)
+    plt.title('Temps de résolution (en ms) en fonction de n sans arc consistance')
+    plt.plot(list(range(1, n_max)), perfs['min_no_AC'])
+    plt.plot(list(range(1, n_max)), perfs['avg_no_AC'])
+    plt.plot(list(range(1, n_max)), perfs['max_no_AC'])
+    plt.legend(handles=[min, avg, max])
+    plt.subplot(212)
+    plt.title('Temps de résolution (en ms) en fonction de n avec arc consistance')
+    plt.plot(list(range(1, n_max)), perfs['min_with_AC'])
+    plt.plot(list(range(1, n_max)), perfs['avg_with_AC'])
+    plt.plot(list(range(1, n_max)), perfs['max_with_AC'])
+    plt.legend(handles=[min, avg, max])
+    plt.figure(2)
+    plt.title('Comparaison des temps de résolution moyens(en ms) avec et sans arc consistance')
+    plt.plot(list(range(1, n_max)), perfs['avg_no_AC'])
+    plt.plot(list(range(1, n_max)), perfs['avg_with_AC'])
+    plt.legend(handles=[no_AC, with_AC])
     plt.show()
 
 
